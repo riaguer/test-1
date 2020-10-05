54ffdf7d1dad1d5450ab70d58ad5d8a87394e7e0
riaguer <adrianlopezbalaguer98@gmail.com>
2020-10-05 19:35:37 +0200

    shame utility clases
    copia de algunas de las clases cs importantes que ya tenia

---
+++ prueba/Geometry.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace If
+{
+    class Geometry
+    {
+        public static double GetVector2DLength(double x, double y)
+        {
+            double aux = x * x + y * y;
+            return System.Math.Sqrt(aux);
+        }
+        public static double GetVector3DLength(double x, double y)
+        {
+            double aux = x * x + y * y;
+            return System.Math.Sqrt(aux);
+        }
+        public static double GetDistance2D(double x1, double y1, double x2, double y2)
+        {
+            double vx = x2 - x1;
+            double vy = y2 - y1;
+            return GetVector2DLength(vx, vy);
+        }
+        public static double AreIntersecting()
+        {
+
+        }
+    }
+}
---
+++ prueba/Misc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If
+{
+    class Misc
+    {
+        public static int GetMinValue(int a , int b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        public static int GetMaxValue(int a,int b)
+        {
+            if (a > b)
+                return (a);
+            else
+                return (b);
+        }
+        //This function return true is the number
+        //is even(par) and false otherwise
+        public static bool IsEven(int value)
+        {
+            if ((value % 2) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsPositive(int value)
+        {
+            if (value >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static double Pi(int value)
+        {
+            double a;
+            return  a = System.Math.PI;
+        }
+        //tienes que hacer una funcion que diga si dos circulos se chocan es decir la suma de los radios Ej:- 5 + 5 = 10 
+        //es mayor que la suma de la distancia de punto central a punto central de cada uno de los circulos entonces 
+        //los circulos se chocan
+    }
+}
